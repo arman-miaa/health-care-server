1d1a52aa4bb7fcb4a8727eacf918ed8f0186142d
Arman Mia <mdarman365506@gmail.com>
2025-12-18 01:27:10 +0600

    fix:neon db connect successfully

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,13 +2,13 @@ import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: './prisma/schema',
+  schema: "./prisma/schema",
   migrations: {
-    path: './prisma/schema/migrations',
- 
+    path: "./prisma/schema/migrations",
+    seed: "npx ts-node src/helpers/seed.ts",
   },
   datasource: {
-    // url: env('DATABASE_URL')
-    url: process.env.DATABASE_URL as string,
-  }
+    url: env('DATABASE_URL')
+    // url: process.env.DATABASE_URL as string,
+  },
 });
